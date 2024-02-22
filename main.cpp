c2918a640d9064769a66de3d015a03a328203709
Jianqiao Han <hanjianqiao@gmail.com>
2024-02-22 18:05:49 +0800

    add PROJECT_IS_TOP_LEVEL，顶层项目才构建例子

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
-#include "samplewindow.h"
 #include <QApplication>
-#include "logger.h"
+
+#include "logger.h" // 这里引入头文件
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    // 使用这个函数启用功能
     qInstallMessageHandler(h::Logger::messageHandler);
 
+    // 然后在项目内就可以使用输出了
     qDebug() << "这是测试输出";
     qCritical() << "This is a critical message!";
-
-    SampleWindow w;
-    w.show();
+    qWarning() << "添加一条警告信息！";
 
     return a.exec();
 }

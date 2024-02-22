4f37c6d23a30a71eef07240649db8ef0ca3266df
Jianqiao Han <hanjianqiao@gmail.com>
2024-02-22 18:12:31 +0800

    update sample

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <QApplication>
-
+#include "samplewindow.h"
 #include "logger.h" // 这里引入头文件
 
 int main(int argc, char *argv[])
@@ -13,6 +13,9 @@ int main(int argc, char *argv[])
     qDebug() << "这是测试输出";
     qCritical() << "This is a critical message!";
     qWarning() << "添加一条警告信息！";
+
+    SampleWindow w;
+    w.show();
 
     return a.exec();
 }
